c8fce50ee5e4127ed1747a05078c56044c251057
Fabiola <saga.fabiola@gmail.com>
2019-09-21 07:09:23 -0500

    Set random color for labels

--- ScatterAndGather/ScatterAndGather/ViewController.swift
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     var isScattered: Bool = false
     let lambdaLogo = UIImageView()
+    let randomColor = UIColor.random
     
     let letterEl = UILabel()
     let letterA = UILabel()
@@ -62,22 +63,22 @@ class ViewController: UIViewController {
     func labelInit(label: UILabel) {
         switch label {
         case letterEl:
-            label.frame = CGRect(x: 5, y: 100, width: 80, height: 80)
+            label.frame = CGRect(x: 5, y: 100, width: 70, height: 80)
             label.text = "L"
         case letterA:
-            label.frame = CGRect(x: 65, y: 100, width: 80, height: 80)
+            label.frame = CGRect(x: 65, y: 100, width: 70, height: 80)
             label.text = "A"
         case letterM:
-            label.frame = CGRect(x: 135, y: 100, width: 80, height: 80)
+            label.frame = CGRect(x: 135, y: 100, width: 70, height: 80)
             label.text = "M"
         case letterB:
-            label.frame = CGRect(x: 200, y: 100, width: 80, height: 80)
+            label.frame = CGRect(x: 200, y: 100, width: 70, height: 80)
             label.text = "B"
         case letterD:
-            label.frame = CGRect(x: 260, y: 100, width: 80, height: 80)
+            label.frame = CGRect(x: 260, y: 100, width: 70, height: 80)
             label.text = "D"
         case letterLastA:
-            label.frame = CGRect(x: 320, y: 100, width: 80, height: 80)
+            label.frame = CGRect(x: 320, y: 100, width: 70, height: 80)
             label.text = "A"
         default:
             print("Error")
@@ -104,6 +105,14 @@ class ViewController: UIViewController {
         
     }
     
+    func setRandomColor() -> UIColor {
+        let red:CGFloat = CGFloat(drand48())
+        let green:CGFloat = CGFloat(drand48())
+        let blue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red:red, green: green, blue: blue, alpha: 1.0)
+    }
+    
     // MARK: Animations
     
     func logoFadeOut() {
@@ -122,17 +131,33 @@ class ViewController: UIViewController {
         UILabel.animate(withDuration: 3.0) {
             label.transform = CGAffineTransform(rotationAngle: CGFloat.random(in: 1...100))
             label.transform = CGAffineTransform(translationX: CGFloat.random(in: 1...700), y: CGFloat.random(in: 1...700))
+            label.backgroundColor = self.setRandomColor()
+            label.textColor = self.setRandomColor()
         }
+
     }
     
     func gatherAnimation(label: UILabel) {
         UILabel.animate(withDuration: 3.0) {
             label.transform = .identity
+            label.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+            label.textColor = .black
         }
-        
     }
    
     
 
+}
+
+extension CGFloat {
+    static var random: CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(red: .random, green: .random, blue: .random, alpha: 1.0)
+    }
 }
 
